998d63abd49a0cdc11aa73bb8bd1d38f75ffadca
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-27 15:40:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,7 @@ class Transfer
   # your code here
   attr_accessor :sender, :receiver, :amount, :status
   
-  def initialize
+  def initialize(sender, receiver, amount)
+    @sender = sender
   end
 end
